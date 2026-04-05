6b37a003b9c1a13fd1a5e5c089bcac3dc8565248
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2026-04-05 09:08:24 +0530

    Update tamilmv_rss.py

--- tamilmv_rss.py
+++ tamilmv_rss.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 # ================= CONFIG =================
-BASE_URL = "https://www.1tamilmv.immo/"
+BASE_URL = "https://www.1tamilmv.cymru/"
 OUT_FILE = "tamilmv.xml"
 STATE_FILE = "state.json"
 
